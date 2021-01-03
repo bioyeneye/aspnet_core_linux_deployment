a346b71a1479f67bae2bef8403ab316ab36f7e99
bioyeneye <bioyeneye@gmail.com>
2021-01-03 22:16:25 +0100

    commit

---
+++ DeployToLinux/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace DeployToLinux
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbIPPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbIPAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbAppDll = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbAppFolderName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbAppBinaryHome = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbAppDescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbAppName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbIPPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbIPAddress);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(474, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Deployement Environment";
+            // 
+            // tbIPPort
+            // 
+            this.tbIPPort.Location = new System.Drawing.Point(244, 44);
+            this.tbIPPort.Name = "tbIPPort";
+            this.tbIPPort.Size = new System.Drawing.Size(217, 23);
+            this.tbIPPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP Port:";
+            // 
+            // tbIPAddress
+            // 
+            this.tbIPAddress.Location = new System.Drawing.Point(10, 44);
+            this.tbIPAddress.Name = "tbIPAddress";
+            this.tbIPAddress.Size = new System.Drawing.Size(217, 23);
+            this.tbIPAddress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbAppDll);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.tbAppFolderName);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.tbAppBinaryHome);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.tbAppDescription);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tbAppName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft YaHei", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(11, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(474, 352);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Application Information";
+            // 
+            // tbAppDll
+            // 
+            this.tbAppDll.Location = new System.Drawing.Point(12, 315);
+            this.tbAppDll.Name = "tbAppDll";
+            this.tbAppDll.Size = new System.Drawing.Size(453, 23);
+            this.tbAppDll.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "App DLL:";
+            // 
+            // tbAppFolderName
+            // 
+            this.tbAppFolderName.Location = new System.Drawing.Point(9, 259);
+            this.tbAppFolderName.Name = "tbAppFolderName";
+            this.tbAppFolderName.Size = new System.Drawing.Size(453, 23);
+            this.tbAppFolderName.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "App Folder Name:";
+            // 
+            // tbAppBinaryHome
+            // 
+            this.tbAppBinaryHome.Location = new System.Drawing.Point(9, 200);
+            this.tbAppBinaryHome.Name = "tbAppBinaryHome";
+            this.tbAppBinaryHome.Size = new System.Drawing.Size(453, 23);
+            this.tbAppBinaryHome.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "App Binary Default:";
+            // 
+            // tbAppDescription
+            // 
+            this.tbAppDescription.Location = new System.Drawing.Point(10, 102);
+            this.tbAppDescription.Multiline = true;
+            this.tbAppDescription.Name = "tbAppDescription";
+            this.tbAppDescription.Size = new System.Drawing.Size(453, 63);
+            this.tbAppDescription.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "App Description";
+            // 
+            // tbAppName
+            // 
+            this.tbAppName.Location = new System.Drawing.Point(10, 44);
+            this.tbAppName.Name = "tbAppName";
+            this.tbAppName.Size = new System.Drawing.Size(453, 23);
+            this.tbAppName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "App Name:";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(162, 469);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(146, 39);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create/Update";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(10, 469);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(146, 39);
+            this.btnPreview.TabIndex = 6;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // mainForm
+            // 
+            this.AcceptButton = this.btnPreview;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 515);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Deploy Core on Linux";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbIPPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbIPAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbAppDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbAppName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbAppBinaryHome;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbAppFolderName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbAppDll;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnPreview;
+    }
+}
+
---
+++ DeployToLinux/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DeployToLinux
+{
+    public partial class mainForm : Form
+    {
+        String preview = null;
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            var sampleFilePath = @"Data\SmapleConfig.txt";
+            if (!File.Exists(sampleFilePath))
+            {
+                MessageBox.Show("Sample config file is not available");
+                Application.Exit();
+            }
+
+            var tbs = new List<TextBox> { tbAppBinaryHome, tbAppDescription, tbAppDll, tbAppFolderName, tbAppName, tbIPAddress, tbIPPort, };
+            if (CheckTextBoxValue(tbs))
+            {
+                MessageBox.Show("All entries is required");
+                return;
+            }
+
+            string text = File.ReadAllText(sampleFilePath);
+            preview = text.Replace("{{home}}", tbAppBinaryHome.Text)
+                .Replace("{{ip}}", tbIPAddress.Text)
+                .Replace("{{port}}", tbIPPort.Text)
+                .Replace("{{appname}}", tbAppName.Text.ToLower().Trim().Replace(" ", "_"))
+                .Replace("{{Description}}", tbAppDescription.Text)
+                .Replace("{{foldername}}", tbAppFolderName.Text)
+                .Replace("{{appdll}}", tbAppDll.Text);
+            
+            var createFile = MessageBox.Show(preview, "Configuration", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (createFile == DialogResult.Yes)
+            {
+                string strPath = Environment.GetFolderPath(
+                         System.Environment.SpecialFolder.DesktopDirectory);
+
+                if (!Directory.Exists(Path.Combine(strPath, $@"ConfigFolder")))
+                {
+                    Directory.CreateDirectory(Path.Combine(strPath, $@"ConfigFolder"));
+                }
+
+                string createFilePath = Path.Combine(strPath, $@"ConfigFolder\{tbAppName.Text.ToLower().Trim().Replace(" ", "_")}.txt");
+                FileStream fs = new FileStream(createFilePath, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.BaseStream.Seek(0, SeekOrigin.End);
+                sw.WriteLine(preview);
+                sw.Flush();
+                sw.Close();
+                MessageBox.Show("Completed");
+            }
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(preview);
+        }
+
+        bool CheckTextBoxValue(List<TextBox> textBoxes)
+        {
+            foreach (var textBox in textBoxes)
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
